895a3c11e69e96a9137c9449c91fba444f4583e8
gokulnath <localhostgokul@gmail.com>
2026-05-29 19:34:59 +0530

    feat: setup mock data

---
+++ frontEnd/src/data/mockNotes.ts
@@ -0,0 +1,35 @@
+import type { Note } from '../types/@types.note';
+
+export const mockNotes: Note[] = [
+    {
+        id: "random-id-1",
+        title: "Note 1",
+        content: "Hi, I am a note. You can edit me, move me around, and change my color. I will save automatically.",
+        color: {
+            "id": "color-yellow",
+            "colorHeader": "#FFEFBE",
+            "colorBody": "#FFF5DF",
+            "colorText": "#18181A"
+        },
+        position: {
+            x: 100,
+            y: 150
+        },
+    },{
+        id: "random-id-2",
+        title: "Note 2",
+        content: "Halloween is coming! Time to decorate your notes with spooky colors.",
+        color: {
+            id: "color-purple",
+            colorHeader: "#FED0FD",
+            colorBody: "#FEE5FD",
+            colorText: "#18181A",
+        },
+        position: {
+            x: 53,
+            y: 100
+        },
+    },
+];
+
+export default mockNotes;

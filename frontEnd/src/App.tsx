93b11449b05005991fa1d4209519fd890456b2bf
gokulnath <localhostgokul@gmail.com>
2026-06-01 16:23:17 +0530

    feat: add mobile compatibility warning

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,12 +1,20 @@
+import { isMobile, MobileView } from 'react-device-detect'
+import NotesProvider from './context/NotesProvider'
+import NotePage from './pages/NotePage'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    isMobile ? <MobileView style={{ margin: 10, textAlign: 'center' }}>
+      <h2>Not Compatible with mobile screens..</h2>
+    </MobileView> :
+    <div id='app'>
+      <NotesProvider>
+        <NotePage/>
+      </NotesProvider>
+    </div>
   )
 }
 
-export default App
+export default App;
